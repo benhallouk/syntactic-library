40c303212a1e3f702a3edaaca2122978a514e841
Amine Benhallouk <benhallouk@gmail.com>
2016-06-16 12:50:01 +0100

    add lombok for easy auto properties

--- src/main/java/com/syntactic/models/LibraryItem.java
+++ src/main/java/com/syntactic/models/LibraryItem.java
@@ -1,9 +1,11 @@
 package com.syntactic.models;
 
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class LibraryItem
 {
     @Id
@@ -11,49 +13,4 @@ public class LibraryItem
     private String title;
     private String author;
     private String category;
-
-    public LibraryItem()
-    {
-
-    }
-
-    public LibraryItem(Long id,String title,String author,String category)
-    {
-        this.id=id;
-        this.title=title;
-        this.author=author;
-        this.category=category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 }
